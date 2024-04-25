72b007ea4be0fbf25ea47d69946aac41786c05ea
PrateekTechmatys <prateeky562@gmail.com>
2024-04-25 16:25:00 +0530

    section one is done

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -4,6 +4,7 @@ import Profile from './Section1/Profile';
 import Based from './Section1/Based';
 import Tech from './Section1/Tech';
 import Education from './Section1/Education';
+import Experience from './Section1/Experience';
 
 const Section1 = () => {
   return (
@@ -13,6 +14,7 @@ const Section1 = () => {
       <Based/>
       <Tech/>
       <Education/>
+      <Experience/>
    </div>
   );
 }
--- src/components/Section1/Experience.jsx
+++ src/components/Section1/Experience.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-const Education = () => {
+const Experience = () => {
     return (
         <div className='flex flex-col justify-around w-[80%] rounded-lg items-center mb-3 mt-7 p-2 bg-white gap-6 shadow-md'>
-            <div className="flex flex-col gap-4 w-full">
+            <div className="flex flex-col gap-4 w-full"> 
                 <div className="mx-3">
-                    <h5 className="text-lg  text-center text-base-content opacity-70">Education</h5>
+                    <h5 className="text-lg  text-center text-base-content opacity-70">Experience</h5>
                 </div>
                 <div className="text-base-content text-opacity-60">
                     <ol className="relative border-l border-gray-400 border-opacity-30 my-2 mx-4 list-none">
                         <li className="mb-5 ml-4">
                             <div className="absolute w-2 h-2 bg-gray-400 rounded-full border border-gray-400 mt-1.5 left-[-10px]"></div>
-                            <div className="my-0.5 text-xs text-gray-600">2020 - 2024</div>
-                            <h3 className="font-semibold text-gray-800">B.Tech</h3>
-                            <div className="mb-4 text-gray-700">Rajkiya Engineering College, Sonbhadra</div>
+                            <div className="my-0.5 text-xs text-gray-600">April 2023 - June 2023</div>
+                            <h3 className="font-semibold text-gray-800">Web Developer Intern</h3>
+                            <div className="mb-4 text-gray-700">Tulip Organisation</div>
                         </li>
                         <li className="mb-5 ml-4">
                             <div className="absolute w-2 h-2 bg-gray-400 rounded-full border border-gray-400 mt-1.5 left-[-10px]"></div>
-                            <div className="my-0.5 text-xs text-gray-600">2018 - 2020</div>
-                            <h3 className="font-semibold text-gray-800">Class XII</h3>
-                            <div className="mb-4 text-gray-700">St. Andrew's School, Agra</div>
+                            <div className="my-0.5 text-xs text-gray-600">November 2023 - January 2024</div>
+                            <h3 className="font-semibold text-gray-800">React.js Intern</h3>
+                            <div className="mb-4 text-gray-700">Techmatys</div>
                         </li>
                     </ol>
                 </div>
@@ -28,4 +28,4 @@ const Education = () => {
     );
 }
 
-export default Education;
+export default Experience;
