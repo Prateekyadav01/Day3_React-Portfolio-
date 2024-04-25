dcdf575def571816efbed8ac219ed0ffb9db9a25
PrateekTechmatys <prateeky562@gmail.com>
2024-04-25 09:31:37 +0530

    done

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Theme from './Section1/Theme';
 import Profile from './Section1/Profile';
+import Based from './Section1/Based';
 
 const Section1 = () => {
   return (
    <div className='w-[40%] items-center flex flex-col gap-2 border border-solid border-red-500'>
       <Theme/>
       <Profile/>
+      <Based/>
    </div>
   );
 }
--- src/components/Section1/Profile.jsx
+++ src/components/Section1/Profile.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import image from "../../assets/IMG_5866.JPG"
+// import image from "../../assets/IMG_5866.JPG"
 const Profile = () => {
     return (
-        <div className='flex flex-col  bg-white w-[70%] rounded-lg items-center mb-3 mt-5 p-2'>
+        <div className='flex flex-col  bg-white w-[80%] rounded-lg items-center mb-3 mt-5 p-2'>
             <div class="mb-8 rounded-full w-32 h-32 ring ring-primary ring-offset-base-100 ring-offset-2">
-                <img src={image} alt="Prateek Yadav"/></div>
-            <h1 className='font-bold text-2xl'>Prateek Yadav</h1>
-            <div className="mt-3 text-base-content text-opacity-60 font-mono flex items-center ">⚡ Senior Software Engineer | Open Source Enthusiast | PHP | Node.js | React ⚡</div>
+                <img src="" alt="Prateek Yadav"/></div>
+            <h1 className='font-bold text-2xl text-base-content opacity-70'>Prateek Yadav</h1>
+            <div className="mt-3 text-base-content text-opacity-60 font-mono  ">⚡ Senior Software Engineer | Open Source Enthusiast | PHP | Node.js | React ⚡</div>
         </div>
     )
 }
