095b772cfdc16961fa49ef23f2926dade1816937
PrateekTechmatys <prateeky562@gmail.com>
2024-04-26 08:49:25 +0530

    profile_Image Updated

--- src/components/Section1/Profile.jsx
+++ src/components/Section1/Profile.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-// import image from "../../assets/IMG_5866.JPG"
-const Profile = () => {
-    return (
-        <div className='flex flex-col  bg-white w-[80%] rounded-lg items-center mb-3 mt-5 p-2'>
-            <div class="mb-8 rounded-full w-32 h-32 ring ring-primary ring-offset-base-100 ring-offset-2">
-                <img src="" alt="Prateek Yadav"/></div>
-            <h1 className='font-bold text-2xl text-base-content opacity-70'>Prateek Yadav</h1>
-            <div className="mt-3 text-base-content text-opacity-60 font-mono  ">⚡ Software Engineer | Open Source Enthusiast | PHP | Node.js | React ⚡</div>
-            <h3>Download Resume</h3>
-        </div>
-    )
-}
+import React from 'react';
+import image from '../../assets/IMG_6449.png';
 
-export default Profile
+const Profile = () => {
+  return (
+    <div className='flex flex-col bg-white w-[80%] rounded-lg items-center mb-3 mt-5 p-2'>
+      <div className="mb-8 rounded-full w-32 h-32 ring ring-black ring-offset-base-500 ring-offset-2 flex items-center justify-center overflow-hidden">
+        <img
+          className="object-cover w-full h-full"
+          src={image}
+          alt="Prateek Yadav"
+        />
+      </div>
+      <h1 className='font-bold text-2xl text-base-content opacity-70'>Prateek Yadav</h1>
+      <div className="mt-3 text-base-content text-opacity-60 font-mono">
+        ⚡ Software Engineer | Open Source Enthusiast | PHP | Node.js | React ⚡
+      </div>
+      <a
+        href="/path-to-your-resume"
+        className="mt-3 text-base-content text-blue-500 underline hover:no-underline"
+      >
+        Download Resume
+      </a>
+    </div>
+  );
+};
+
+export default Profile;
